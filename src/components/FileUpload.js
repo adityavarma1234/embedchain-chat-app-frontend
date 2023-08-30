f5cb1bd4bf70892f6e6ef35a80936d8f88ff2f4f
aditya <aditya.varma@shadowfax.in>
2023-08-30 13:58:41 +0530

    Add support for wrong file format

--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -1,10 +1,28 @@
 // components/FileUpload.js
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { useDropzone } from 'react-dropzone';
-import axios from '../utils/axiosConfig';
+import axios from '../utils/axiosConfig'; // Import the Axios instance
 
-const FileUpload = () => {
+const FileUpload = ({ onFileUpload }) => {
+  const [uploading, setUploading] = useState(false);
+  const [uploadSuccess, setUploadSuccess] = useState(false);
+  const [fileFormatError, setFileFormatError] = useState(false); // State for file format error
+
   const onDrop = useCallback(async (acceptedFiles) => {
+    if (uploading) return;
+
+    // Check if any uploaded file has an invalid format (not TXT or PDF)
+    const invalidFile = acceptedFiles.find(
+      (file) => !['application/pdf', 'text/plain'].includes(file.type)
+    );
+
+    if (invalidFile) {
+      setFileFormatError(true);
+      return;
+    }
+
+    setUploading(true);
+
     const formData = new FormData();
     acceptedFiles.forEach((file) => {
       formData.append('file', file);
@@ -16,24 +34,36 @@ const FileUpload = () => {
           'Content-Type': 'multipart/form-data',
         },
       });
-      console.log('Upload response:', response.data);
-      console.log('Response status:', response.status);
-      // Handle the response from the backend as needed
+
       if (response.status === 200) {
-        // Redirect to the chat page
-        window.location.href = '/chat'; // Change '/chat' to your chat page URL
+        setUploadSuccess(true);
+        setFileFormatError(false); // Reset the file format error state
+        onFileUpload(acceptedFiles);
+        window.location.href = '/chat';
       }
     } catch (error) {
-      console.error('Upload error:', error);
+      console.error('File upload error:', error);
+    } finally {
+      setUploading(false);
     }
-  }, []);
+  }, [onFileUpload, uploading]);
 
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    accept: ['.txt', '.pdf'], // Only accept TXT and PDF files
+  });
 
   return (
-    <div {...getRootProps()} className={`dropzone ${isDragActive ? 'active' : ''}`}>
-      <input {...getInputProps()} />
-      <p>Drag & drop a file here or click to select one</p>
+    <div>
+      <div {...getRootProps()} className={`dropzone ${isDragActive ? 'active' : ''}`}>
+        <input {...getInputProps()} />
+        <p>Drag & drop a TXT or PDF file here or click to select one</p>
+      </div>
+      {fileFormatError && (
+        <p>File format incorrect. Please upload a TXT or PDF file.</p>
+      )}
+      {uploading && <p>Uploading...</p>}
+      {uploadSuccess && <p>File uploaded successfully!</p>}
     </div>
   );
 };
